f5a92f0af519992f20409e7ce2ac203c8d3bd40d
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2024-04-20 23:14:29 +0530

    Added Sign in and sign up forms for authentication

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,7 @@ class MyApp extends StatelessWidget {
           appBarTheme: const AppBarTheme(
               elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello"),
-        ),
-        body: Column(
-          children: [
-            const Center(
-              child: Text("Azon Home Page"),
-            ),
-            Builder(builder: (context) {
-              return ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AuthScreen.routeName);
-                  },
-                  child: Text("Click"));
-            })
-          ],
-        ),
-      ),
+      home: AuthScreen(),
     );
   }
 }
